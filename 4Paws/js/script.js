5b05076ddf3b80bea9821598b6f16a1f72797c79
Rasa50 <rex_id@upi.edu>
2025-10-08 20:38:35 +0700

    add file

---
+++ 4Paws/js/script.js
@@ -0,0 +1,14 @@
+// Toggle Class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#humburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const humburger = document.querySelector("#humburger-menu");
+document.addEventListener("click", function (e) {
+  if (!humburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
